678d7a13057233000b1ca68e0129cb1b0fadce66
Saffron-28 <saffron.simpson@ada.ac.uk>
2022-11-04 12:40:44 +0000

    difference from answer function

--- main.py
+++ main.py
@@ -3,11 +3,21 @@
 import random 
 
 num = 0
+player_guess = int(input("please guess a number?"))
 
 def random_number_generator():
   num = random.randint(0,20)
   return num
-  
 
-print(random_number_generator())
+# print(random_number_generator())
+
+def difference_from_answer(num,player_guess):
   
+  if player_guess < num:
+    return ('tooo low')
+  elif player_guess > num:
+    return ('tooo high')
+  else:
+    return ('match whooo')
+
+print(difference_from_answer(num,player_guess))
